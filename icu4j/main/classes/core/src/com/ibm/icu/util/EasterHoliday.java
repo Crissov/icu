7e850898a6aa82a74b356297edb5d92bc7992f24
Christoph Päper <bugzilla@crissov.de>
2020-06-02 15:39:44 +0200

    Update EasterHoliday.java
    - add the same holidays as supported on Wikipedia: <https://en.wikipedia.org/wiki/Module:Easter>

--- icu4j/main/classes/core/src/com/ibm/icu/util/EasterHoliday.java
+++ icu4j/main/classes/core/src/com/ibm/icu/util/EasterHoliday.java
@@ -55,7 +55,7 @@ public class EasterHoliday extends Holiday
      * or Orthodox calendar.
      *
      * @param daysAfter The number of days before (-) or after (+) Easter
-     * @param orthodox  Use the Orthodox calendar?
+     * @param orthodox  Use the Orthodox Computus?
      * @param name      The name of the holiday
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
@@ -66,88 +66,242 @@ public class EasterHoliday extends Holiday
     }
 
     /**
-     * Shrove Tuesday, aka Mardi Gras, 48 days before Easter
+     * Septuagesima Sunday, literally 70 days but actually 9 weeks before Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday SHROVE_TUESDAY  = new EasterHoliday(-48,    "Shrove Tuesday");
+    static public final EasterHoliday SEPTUAGESIMA_SUNDAY  = new EasterHoliday(-63,    "Septuagesima");
+
+    /**
+     * Sexagesima Sunday, literally 60 days but actually 8 weeks before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday SEXAGESIMA_SUNDAY    = new EasterHoliday(-56,    "Sexagesima");
+
+    /**
+     * Shrove or Fat Thursday (Carnival start), 52 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday SHROVE_THURSDAY      = new EasterHoliday(-52,    "Shrove Thursday");
+
+    /**
+     * Quinquagesima or Shrove Sunday, aka Estomihi, literally 50 days but actually 7 weeks before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday QUINQUAGESIMA_SUNDAY = new EasterHoliday(-49,    "Quinquagesima");
+
+    /**
+     * Rose or Shrove Monday, 49 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday SHROVE_MONDAY        = new EasterHoliday(-48,    "Rose Monday");
+
+    /**
+     * Shrove Tuesday, aka Mardi Gras, Carnival or Pancake Day, 48 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday SHROVE_TUESDAY       = new EasterHoliday(-47,    "Shrove Tuesday");
 
     /**
      * Ash Wednesday, start of Lent, 47 days before Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday ASH_WEDNESDAY   = new EasterHoliday(-47,    "Ash Wednesday");
+    static public final EasterHoliday ASH_WEDNESDAY        = new EasterHoliday(-46,    "Ash Wednesday");
 
     /**
-     * Palm Sunday, 7 days before Easter
+     * Invocabit Sunday, 6 weeks before Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday PALM_SUNDAY     = new EasterHoliday( -7,    "Palm Sunday");
+    static public final EasterHoliday INVOCABIT_SUNDAY     = new EasterHoliday(-42,    "Invocabit");
 
     /**
-     * Maundy Thursday, 3 days before Easter
+     * Reminiscere Sunday, 5 weeks before Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday MAUNDY_THURSDAY = new EasterHoliday( -3,    "Maundy Thursday");
+    static public final EasterHoliday REMINISCERE_SUNDAY   = new EasterHoliday(-35,    "Reminiscere");
 
     /**
-     * Good Friday, 2 days before Easter
+     * Oculi Sunday, 28 days before Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday GOOD_FRIDAY     = new EasterHoliday( -2,    "Good Friday");
+    static public final EasterHoliday OCULI_SUNDAY         = new EasterHoliday(-28,    "Oculi");
 
     /**
-     * Easter Sunday
+     * Motering Sunday, 3 weeks before Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday EASTER_SUNDAY   = new EasterHoliday(  0,    "Easter Sunday");
+    static public final EasterHoliday MOTHERING_SUNDAY     = new EasterHoliday(-21,    "Mothering Sunday");
+
+    /**
+     * Passion or Laetare Sunday, aka Christian Passa, 2 weeks before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday PASSION_SUNDAY       = new EasterHoliday(-14,    "Passion Sunday");
+
+    /**
+     * Palm Sunday, 1 week before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday PALM_SUNDAY          = new EasterHoliday( -7,    "Palm Sunday");
+
+    /**
+     * Holy Monday, 6 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday HOLY_MONDAY          = new EasterHoliday( -6,    "Holy Monday");
+
+    /**
+     * Holy Tuesday, 5 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday HOLY_TUESDAY         = new EasterHoliday( -5,    "Holy Tuesday");
+
+    /**
+     * Holy or Spy Wednesday, 4 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday HOLY_WEDNESDAY       = new EasterHoliday( -4,    "Holy Wednesday");
+
+    /**
+     * Maundy Thursday, aka Last Supper, 3 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday MAUNDY_THURSDAY      = new EasterHoliday( -3,    "Maundy Thursday");
+
+    /**
+     * Good Friday, aka Crucifixion, 2 days before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday GOOD_FRIDAY          = new EasterHoliday( -2,    "Good Friday");
+
+    /**
+     * Holy Saturday, 1 day before Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday HOLY_SATURDAY        = new EasterHoliday( -1,    "Holy Saturday");
+
+    /**
+     * Easter Sunday, aka Resurrection
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday EASTER_SUNDAY        = new EasterHoliday(  0,    "Easter Sunday");
 
     /**
      * Easter Monday, 1 day after Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday EASTER_MONDAY   = new EasterHoliday(  1,    "Easter Monday");
+    static public final EasterHoliday EASTER_MONDAY        = new EasterHoliday(  1,    "Easter Monday");
 
     /**
-     * Ascension, 39 days after Easter
+     * Divine Mercy Sunday, 1 week after Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday ASCENSION       = new EasterHoliday( 39,    "Ascension");
+    static public final EasterHoliday DIVINE_MERCY_SUNDAY  = new EasterHoliday(  7,    "Divine Mercy");
 
     /**
-     * Pentecost (aka Whit Sunday), 49 days after Easter
+     * Misericordias Domini Sunday, 2 weeks after Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday PENTECOST       = new EasterHoliday( 49,    "Pentecost");
+    static public final EasterHoliday MISERICORDIAS_SUNDAY = new EasterHoliday( 14,    "Misericordias Domini");
 
     /**
-     * Whit Sunday (aka Pentecost), 49 days after Easter
+     * Jubilate Sunday, 3 weeks after Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday WHIT_SUNDAY     = new EasterHoliday( 49,    "Whit Sunday");
+    static public final EasterHoliday JUBILATE_SUNDAY      = new EasterHoliday( 21,    "Jubilate");
+
+    /**
+     * Cantate Sunday, 4 weeks after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday CANTATE_SUNDAY       = new EasterHoliday( 28,    "Cantate");
+
+    /**
+     * Vocem Jucunditatis Sunday, 5 weeks after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday VOCEM_SUNDAY         = new EasterHoliday( 35,    "Vocem jucunditatis");
+
+    /**
+     * Ascension Thursday, 39 days after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday ASCENSION            = new EasterHoliday( 39,    "Ascension");
+
+    /**
+     * Pentecost, aka Whit or Exaudi Sunday, 7 weeks after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday PENTECOST            = new EasterHoliday( 49,    "Pentecost");
+
+    /**
+     * Whit Sunday, aka Pentecost, 7 weeks after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday WHIT_SUNDAY          = new EasterHoliday( 49,    "Whit Sunday");
 
     /**
      * Whit Monday, 50 days after Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday WHIT_MONDAY     = new EasterHoliday( 50,    "Whit Monday");
+    static public final EasterHoliday WHIT_MONDAY          = new EasterHoliday( 50,    "Whit Monday");
 
     /**
-     * Corpus Christi, 60 days after Easter
+     * Trinity Sunday, 8 weeks after Easter
      * @draft ICU 2.8
      * @provisional This API might change or be removed in a future release.
      */
-    static public final EasterHoliday CORPUS_CHRISTI  = new EasterHoliday( 60,    "Corpus Christi");
+    static public final EasterHoliday TRINITY_SUNDAY       = new EasterHoliday( 28,    "Trinity Sunday");
+
+    /**
+     * Corpus Christi, aka Body and Blood, 60 days after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday CORPUS_CHRISTI       = new EasterHoliday( 60,    "Corpus Christi");
+
+    /**
+     * Sacred Heart Friday, 68 days after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday SACRED_HEART         = new EasterHoliday( 68,    "Sacred Heart");
+
+    /**
+     * Immaculate Heart Saturday, 69 days after Easter
+     * @draft ICU 2.8
+     * @provisional This API might change or be removed in a future release.
+     */
+    static public final EasterHoliday IMMACULATE_HEART     = new EasterHoliday( 69,    "Immaculate Heart");
 }
 
 class EasterRule implements DateRule {
@@ -253,21 +407,21 @@ class EasterRule implements DateRule {
             cal.setTime(date);
 
             int year = cal.get(Calendar.YEAR);
-            int g = year % 19;  // "Golden Number" of year - 1
-            int i = 0;          // # of days from 3/21 to the Paschal full moon
-            int j = 0;          // Weekday (0-based) of Paschal full moon
+            int g = year % 19;  // "Golden Number" of year minus 1 (Metonic cycle)
+            int i = 0;          // # of days from 21 March to the Paschal full moon
+            int j = 0;          // Weekday (0 = Sunday) of Paschal full moon
 
             if (cal.getTime().after( cal.getGregorianChange()))
             {
                 // We're past the Gregorian switchover, so use the Gregorian rules.
-                int c = year / 100;
+                int c = year / 100; // century
                 int h = (c - c/4 - (8*c+13)/25 + 19*g + 15) % 30;
                 i = h - (h/28)*(1 - (h/28)*(29/(h+1))*((21-g)/11));
                 j = (year + year/4 + i + 2 - c + c/4) % 7;
             }
             else
             {
-                // Use the old Julian rules.
+                // Use the orthodox rules with the Julian leap rule.
                 i = (19*g + 15) % 30;
                 j = (year + year/4 + i) % 7;
             }
